052e5e9d496a5a5acc7f4fc8e447693042b9454d
Raphael Yazdi <raphael@voodoo.io>
2020-01-03 17:49:31 +0100

    Implements methods

--- src/DynamoTools.js
+++ src/DynamoTools.js
@@ -9,8 +9,8 @@ const CLASS_NAME = 'DynamoTools';
 class DynamoTools {
     /**
      * Constructor
-     * 
-     * @param {Object} cli - aws cli object, instance of aws-sdk 
+     *
+     * @param {Object} cli - aws cli object, instance of aws-sdk
      * @param {Object} opt - (optional) options of dynamotools
      * @param {Number} opt.retryMax - number of retry per dynamo call
      */
@@ -37,8 +37,106 @@ class DynamoTools {
     }
 
     /**
+     * Make a query with conditions in dynamoDB
+     *
+     * @param {string} dynamoTable - The name of the dynamo table
+     * @param customParams - query with the conditions
+     * @returns results
+     */
+    query(dynamoTable, customParams, response = [], startKey = null) {
+        return new Promise((resolve,reject) => {
+            if(typeof tableName !== 'string' ||
+              !Array.isArray(customParams.conditions)) {
+                let myErr = new Error('BAD_PARAM');
+                myErr.moreInfo = {
+                    from: CLASS_NAME,
+                    params: {
+                        function_name: 'query',
+                        dynamo_table: dynamoTable,
+                        partition_key: customParams
+                    }
+                };
+                return reject(myErr)
+            }
+
+            let params = {
+                TableName: dynamoTable
+            };
+
+            if(startKey) params.ExclusiveStartKey = startKey;
+            let keyCondition = '';
+            let keyAttribute = {};
+            let keyName = {};
+            for(const [index, condition] of customParams.conditions.entries()) {
+                if(keyCondition !== '') keyCondition += ' and ';
+                keyCondition += `#i_${index} ${condition.operator} `;
+                if(Array.isArray(condition.value)) {
+                    keyCondition += ':b1 AND :b2';
+                    keyAttribute[':b1'] = condition.value[0];
+                    keyAttribute[':b2'] = condition.value[1]
+                }
+                else {
+                    keyCondition += `:i_${index}`;
+                    keyAttribute[`:i_${index}`] = condition.value
+                }
+                keyName[`#i_${index}`] = condition.key
+            }
+
+            if(keyCondition !== '') {
+                params.KeyConditionExpression = keyCondition;
+                params.ExpressionAttributeValues = keyAttribute;
+                params.ExpressionAttributeNames = keyName
+            }
+
+            this.cli.query(params, async (err,data) => {
+                if (err) {
+                    if (err.retryable) {
+                        try {
+                            const data = await utils.retry(this.cli.query.bind(this.cli), params, this.retryMax);
+
+                            if(!data.Items) return resolve(response);
+                            for(const item of data.Items) {
+                                response.push(item)
+                            }
+                            if(data.LastEvaluatedKey) return this.query(dynamoTable, params, response, data.LastEvaluatedKey);
+                            return resolve(response)
+                        } catch (err) {
+                            err.moreInfo = {
+                                from: CLASS_NAME,
+                                params: {
+                                    function_name: 'query',
+                                    dynamo_table: dynamoTable,
+                                    params: params
+                                }
+                            };
+                            return reject(err)
+                        }
+                    }
+                    err.moreInfo = {
+                        from: CLASS_NAME,
+                        params: {
+                            function_name: 'query',
+                            dynamo_table: dynamoTable,
+                            params: params
+                        }
+                    };
+                    return reject(err)
+                }
+
+                if(!data.Items) return resolve(response);
+                for(const item of data.Items) {
+                    response.push(item)
+                }
+                if(data.LastEvaluatedKey) return this.query(dynamoTable, params, response, data.LastEvaluatedKey);
+                return resolve(response)
+            })
+        })
+    }
+
+
+    /**
      * This function will put an item to dynamoDB
-     *  
+     *
      * @param {string} dynamoTable - The name of the dynamoDB table
      * @param {object} item - The item to push to dynamo DB
      */
@@ -62,7 +160,7 @@ class DynamoTools {
             const param = {
                 TableName: dynamoTable,
                 Item: item
-            }
+            };
 
             this.cli.put(param, async (err) => {
                 if (err) {
@@ -98,7 +196,7 @@ class DynamoTools {
      * @param results
      */
     scan(dynamoTable, hashKeyName, hashKeyValue, exclusiveStartKey = null, limit = null, params = {}, results = {Items: []}, cli = null) {
-        return this._promiseFunction('scan', 'scan', dynamoTable, hashKeyName, hashKeyValue, exclusiveStartKey, limit, params, results, cli); 
+        return this._promiseFunction('scan', 'scan', dynamoTable, hashKeyName, hashKeyValue, exclusiveStartKey, limit, params, results, cli);
     }
 
     /**
@@ -119,7 +217,7 @@ class DynamoTools {
 
         return new Promise((resolve, reject) => {
             if (typeof dynamoTable !== 'string' || typeof partitionKeys !== 'object' || !Array.isArray(partitionKeys)) {
-                let myErr = new Error('BAD_PARAM')
+                let myErr = new Error('BAD_PARAM');
                 myErr.moreInfo = errObj;
                 return reject(myErr)
             }
@@ -130,7 +228,7 @@ class DynamoTools {
                         Keys: partitionKeys
                     }
                 }
-            }
+            };
 
             this.cli.batchGet(param, async (err, data) => {
                 if (err) {
@@ -153,15 +251,18 @@ class DynamoTools {
     }
 
     _formatRes(data, dynamoTable) {
-        if (data.hasOwnProperty('Responses') && data.Responses.hasOwnProperty(dynamoTable) && Array.isArray(data.Responses[dynamoTable])) return data.Responses[dynamoTable];
+
+        if (Object.prototype.hasOwnProperty.call(data, 'Responses') &&
+          Object.prototype.hasOwnProperty.call(data.Responses, dynamoTable) &&
+          Array.isArray(data.Responses[dynamoTable])) return data.Responses[dynamoTable];
         return [];
-    } 
+    }
 
     /**
      * This method will get a specific object from dynamo
-     * 
-     * @param {string} dynamoTable 
-     * @param {Object} partitionKey 
+     *
+     * @param {string} dynamoTable
+     * @param {Object} partitionKey
      */
     getItem(dynamoTable, partitionKey) {
         const errObj = {
@@ -173,14 +274,14 @@ class DynamoTools {
         };
         return new Promise((resolve, reject) => {
             if (typeof dynamoTable !== 'string' || typeof partitionKey !== 'object' || Array.isArray(partitionKey)) {
-                let myErr = new Error('BAD_PARAM')
+                let myErr = new Error('BAD_PARAM');
                 myErr.moreInfo = errObj;
                 return reject(myErr);
             }
             const param = {
                 TableName: dynamoTable,
                 Key: partitionKey
-            }
+            };
 
             this.cli.get(param, async (err, data) => {
                 if (err) {
@@ -203,30 +304,119 @@ class DynamoTools {
     }
 
     /**
+     * Update items into DynamoDB
+     *
+     * @param {string} dynamoTable - The name of the dynamo table
+     * @param {object} params - The entry params
+     */
+    updateItem(dynamoTable, params) {
+        return new Promise((resolve, reject) => {
+            if (typeof dynamoTable !== 'string' || typeof params !== 'object' || Array.isArray(params)) {
+                let myErr = new Error('BAD_PARAM');
+                myErr.more_infos_updateItem = {
+                    from: CLASS_NAME,
+                    params: {
+                        dynamo_table: dynamoTable,
+                        entry_param: params
+                    }
+                };
+                return reject(myErr)
+            }
+
+            let param = {
+                TableName: dynamoTable,
+                Key: params.key,
+                UpdateExpression: '',
+                ExpressionAttributeValues: {}
+            };
+
+            let firstSet = true;
+            let firstAdd = true;
+            for (const key in params.set) {
+                if (param.UpdateExpression === '') param.UpdateExpression += 'set';
+                if (firstSet) {
+                    param.UpdateExpression += ` ${key} = :${key}`;
+                    firstSet = false
+                } else {
+                    param.UpdateExpression += `,${key} = :${key}`
+                }
+                param.ExpressionAttributeValues[`:${key}`] = params.set[key]
+            }
+
+            for (const key in params.add) {
+                if (param.UpdateExpression.indexOf('add') === -1) {
+                    if (param.UpdateExpression === '') param.UpdateExpression += 'add';
+                    else param.UpdateExpression += ' add'
+                }
+
+                if (firstAdd) {
+                    param.UpdateExpression += ` ${key} :${key}`;
+                    firstAdd = false
+                } else {
+                    param.UpdateExpression += `,${key} :${key}`
+                }
+                param.ExpressionAttributeValues[`:${key}`] = params.add[key]
+            }
+
+            this.cli.update(param, async (err) => {
+                if (err) {
+                    if (err.retryable) {
+                        try {
+                            await utils.retry(this.cli.update.bind(this.cli), param, this.retryMax);
+                            return resolve()
+                        } catch (err) {
+                            err.more_infos_updateItem = {
+                                from: CLASS_NAME,
+                                params:{
+                                    dynamo_table: dynamoTable,
+                                    entry_param: params
+                                }
+                            };
+                            return reject(err)
+                        }
+                    }
+                    err.more_infos_updateItem = {
+                        from: CLASS_NAME,
+                        params: {
+                            dynamo_table: dynamoTable,
+                            entry_param: params
+                        }
+                    };
+                    return reject(err)
+                }
+
+                return resolve()
+            })
+        })
+    }
+
+
+
+    /**
      * Delete a specific item by hashkey
-     * @param {String} table 
-     * @param {String} key 
+     * @param {String} table
+     * @param {String} key
      */
     async deleteItem(table, key) {
         const params = {
             TableName: table,
             Key: key
         };
-        
+
         await this.cli.delete(params).promise();
     }
 
     /**
      * Private method to execute a method from aws sdk as a promise, with retry system.
-     * 
-     * @param {Sting} AWSMethod - method from aws cli to execute 
+     *
+     * @param {String} AWSMethod - method from aws cli to execute
      * @param {String} method - method from dynamotools which is currently executed
-     * @param {String} dynamoTable - name of the dynamo table to use 
+     * @param {String} dynamoTable - name of the dynamo table to use
      * @param {String} hashKeyName - key's name to request
      * @param {String} hashKeyValue - value to request for hashKeyName
      * @param {Object} exclusiveStartKey - use for pagination
-     * @param {Object} results - query's results 
-     * @param {*} cli - fallback cli in case the default one fail, useful when we use DAX and we want to fallback to dynamo 
+     * @param {Object} results - query's results
+     * @param {*} cli - fallback cli in case the default one fail, useful when we use DAX and we want to fallback to dynamo
      * @param {Number} limit - in case of specific limit
      * @param {Object} params - object with the aws-sdk syntax for sending params to dynamo
      */
@@ -254,9 +444,9 @@ class DynamoTools {
                 return reject(myErr);
             }
 
-            if(!params) params = {}
+            if(!params) params = {};
 
-            params.TableName = dynamoTable
+            params.TableName = dynamoTable;
 
             if(limit) {
                 params.Limit = limit
@@ -321,31 +511,31 @@ class DynamoTools {
 
     /**
      * Internal method to retry a method and collect its data.
-     * 
-     * @param {String} func - current method of dynamotools which is executed 
+     *
+     * @param {String} func - current method of dynamotools which is executed
      * @param {String} cliFunc - aws cli method to execute
-     * @param {String} dynamoTable - name of the dynamo table to use 
+     * @param {String} dynamoTable - name of the dynamo table to use
      * @param {String} hashKeyName - key's name to request
      * @param {String} hashKeyValue - value to request for hashKeyName
      * @param {Array} results - array of results
      * @param {Object} params - object to send to aws cli method
-     * @param {*} cli - fallback cli 
+     * @param {*} cli - fallback cli
      */
     async _execute(func, cliFunc, dynamoTable, hashKeyName, hashKeyValue, results, params, cli, limit) {
         // retry the method with its original params
         const data = await utils.retry(cli[cliFunc], [params], true, this.retryMax);
         // collect data into results array
-        await this._collecData(func, cliFunc, data, dynamoTable, hashKeyName, hashKeyValue, results, cli, limit, params)
+        await this._collecData(func, cliFunc, data, dynamoTable, hashKeyName, hashKeyValue, results, cli, limit, params);
         return results;
     }
 
     /**
      * Internal method to collect data from aws method and put it into an array.
-     * 
-     * @param {String} func - current method of dynamotools which is executed 
+     *
+     * @param {String} func - current method of dynamotools which is executed
      * @param {String} cliFunc - aws cli method to execute
      * @param {Object} data - object returns by aws sdk
-     * @param {String} dynamoTable - name of the dynamo table to use 
+     * @param {String} dynamoTable - name of the dynamo table to use
      * @param {String} hashKeyName - key's name to request
      * @param {String} hashKeyValue - value to request for hashKeyName
      * @param {Array} results - array of results
